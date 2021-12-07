8cced1184bf8328cfaf55565010c078f0581f22c
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-12-07 12:04:44 -0800

    fetch

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
  class Posts extends Component {
+     componentWillUnmount(){
+         fetch('https://jsonplaceholder.typicode.com/posts')
+         .then(res => res.json())
+         .then(data => console.log(data))
+     }
     render() {
         return (
             <div>
-                
+                <h1>Posts</h1>
             </div>
         )
     }
