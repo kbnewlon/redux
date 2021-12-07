ace32ec070993f49145df294eef45dd1d7fd3ce7
Kayla Newlon <kayla.b.newlon@gmail.com>
2021-12-07 12:00:04 -0800

    posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+import Posts from './components/Posts'
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Posts />
     </div>
   );
 }
